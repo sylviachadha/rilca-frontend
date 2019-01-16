777cc3c6a1c21b29a5fe9175c5c089e637b1dc48
Udorm <phonudorm@gmail.com>
2019-01-16 19:07:09 +0700

    add score calculation

--- routes/documents.js
+++ routes/documents.js
@@ -111,6 +111,79 @@ router.post('/new_document', async (req, res) => {
 
 });
 
+router.get('/calculate_score', async (req, res) => {
+    var doc_ID = req.param('doc_ID');
+    console.log(doc_ID)
+
+    
+    let conn;
+    try {
+        //calculate standard section
+        SSScore = 0;
+        console.log(SSScore)
+        var ssScoreSql = "SELECT SUM(PAline_Score) as 'Score' FROM pa_line WHERE PADoc_ID = '"+ doc_ID +"' AND PAitem_ID BETWEEN 1 AND 90"
+        console.log(ssScoreSql)
+        conn = await pool.getConnection();
+        var rows = await conn.query(ssScoreSql);
+        console.log(rows)
+        console.log(rows[0].Score)
+        SSScore = rows[0].Score
+        console.log("SSScore: " + SSScore)
+
+        //calculate development section
+        DSScore = 0;
+        var dsScoreSql = "SELECT SUM(PAline_Score) as 'Score' FROM pa_line WHERE PADoc_ID = '"+ doc_ID +"' AND PAitem_ID BETWEEN 91 AND 198"
+        console.log(dsScoreSql)
+        rows = await conn.query(dsScoreSql);
+        console.log(rows)
+        DSScore = rows[0].Score
+        console.log("DSSCore: " + DSScore)
+
+        //calculate score of top five staff
+        var Top5Score = [];
+        var sql = "SELECT sum(pa_line.PAline_Score) as 'total_score', staff.Staff_ID " +
+        "FROM pa_line INNER join pa_document on pa_line.PADoc_ID = pa_document.Doc_ID " +
+            "INNER JOIN staff on staff.Staff_ID = pa_document.Acad_ID " +
+        "WHERE pa_document.Doc_year = (SELECT pa_document.Doc_year from pa_document WHERE pa_document.Doc_ID = '"+ doc_ID +"') " +
+        "AND staff.Program_ID = (SELECT staff.Program_ID from staff INNER JOIN pa_document on staff.staff_id = pa_document.Acad_ID WHERE pa_document.Doc_ID = '"+ doc_ID +"') " +
+        "GROUP BY staff.Staff_ID "+
+        "ORDER BY total_score DESC "+
+        "LIMIT 5"
+        rows = await conn.query(sql);
+        for (let row of rows) {
+            Top5Score.push({"total_score": row.total_score, "Staff_ID": row.Staff_ID})
+        }
+        console.log(Top5Score)
+
+        //calculate average of top five staff
+        Top5Average = 0;
+        for (let s of Top5Score){
+            Top5Average += s.total_score;
+        }
+        Top5Average = Top5Average/5;
+        console.log(Top5Average)
+
+        //Calculate the final score
+        final_ssScore =  0;
+        final_dsScore = 0;
+        if (SSScore > 60) {
+            final_ssScore = 60;
+            final_dsScore = (((SSScore - 60) + DSScore)*20)/Top5Average;
+        }else{
+            final_ssScore = SSScore;
+            final_dsScore = (DSScore*20)/Top5Average;
+        }
+        console.log("Final Standard Section Score: " + final_ssScore);
+        console.log("Final Development Section Score: " + final_dsScore);
+        
+    } catch (err) {
+        console.log(err)
+        throw err;
+    } finally {
+        if (conn) return conn.end();
+    }
+});
+
 
 async function setNamesInForm() {
     names = [];
